a70a40431e57cb9696a2d28c099b1e79a793486c
sixto <sixto.cantolla@gmail.com>
2013-07-08 07:15:04 +0200

    Created structure project

---
+++ es.sixto.ValidParentheses-0.0.1-SNAPSHOT/src/main/java/es/sixto/validparenthesses/ValidParenthesses.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package es.sixto.validparenthesses;
+
+/**
+ * @author Sixto
+ * Given a string comprising just of the characters (,),{,},[,] determine if it is well-formed or not.
+ * Your program should accept as its first argument a path to a filename. Each line in this file contains
+ *  a string comprising of the characters mentioned above. e.g.
+ *  ()
+ *  ([)]
+ *  Print out True or False if the string is well-formed e.g.
+ *  True
+ *  False
+ */
+public class ValidParenthesses {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ es.sixto.ValidParentheses-0.0.1-SNAPSHOT/src/test/java/es/sixto/validparenthesses/ValidParenthessesTest.java
@@ -0,0 +1,14 @@
+package es.sixto.validparenthesses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidParenthessesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
